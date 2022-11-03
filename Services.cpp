6505f567e6b2a870721a8a5bbd8674aa87835ad0
rohitrks2003 <75920133+rohitrks2003@users.noreply.github.com>
2022-11-03 16:50:57 +0530

    Add files via upload

---
+++ Services.cpp
@@ -0,0 +1,112 @@
+#include<bits/stdc++.h>
+#include "contact.cpp"
+using namespace std;
+
+class Services
+{
+	public:
+	
+	bool checkValidContactNumber(string contactNumber)
+	{
+		for(auto x:contactNumber)
+		{
+			if(x>='0' and x<='9')
+			{
+				// this is a valid character for a phone number
+			}
+			else 
+			{
+				return false;
+			}
+		}
+		if(contactNumber.length()!=10)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	// Valid name only contains lowercase and uppercase character
+	bool checkValidName(string Name)
+	{
+		for(char character:Name)
+		{
+			if(character>='A' and character<='Z')
+			{
+				
+			}
+			else if(character>='a' and character<='z')
+			{
+				
+			}
+			else 
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	// this function is used to convert a uppercase character in lowercase
+	char convertToLowerCase(char character)
+	{
+		if(character>='A' and character<='Z')
+		{
+			character+=32;
+		}
+		return character;
+	}
+	
+	bool CheckIfCharactersAreSameInCaseInsensitive(char firstCharacter,char secondCharacter)
+	{
+		// convert both the characters in lowercase
+		firstCharacter=convertToLowerCase(firstCharacter);
+		secondCharacter=convertToLowerCase(secondCharacter);
+		return firstCharacter==secondCharacter;
+	}
+	
+	bool checkIfTheStringsHaveSamePrefix(string firstString,string secondString)
+	{
+		if(firstString.length()==0 or secondString.length()==0)
+		{
+			return true;
+		}
+		if(firstString.length()>secondString.length())
+		{
+			return false;
+		}
+		
+		int lengthToCheck=min(firstString.length(),secondString.length());
+		bool stringsHaveSamePrefix=true;
+		for(int i=0;i<lengthToCheck;i++)
+		{
+			if(!CheckIfCharactersAreSameInCaseInsensitive(firstString[i],secondString[i]))
+			{
+				stringsHaveSamePrefix=false;
+				break;
+			}
+		}
+		return stringsHaveSamePrefix;
+	}
+	
+	bool isTheContactMatching(Contact contact,Contact prefixContact)
+	{
+		string firstName=contact.getfirstName();
+		string lastName=contact.getlastName();
+		string contactNumber=contact.getcontactNumber();
+		
+		string firstNamePrefix=prefixContact.getfirstName();
+		string lastNamePrefix=prefixContact.getlastName();
+		string contactNumberPrefix=prefixContact.getcontactNumber();
+
+		if(checkIfTheStringsHaveSamePrefix(firstNamePrefix,firstName) and checkIfTheStringsHaveSamePrefix(lastNamePrefix,lastName)
+		and checkIfTheStringsHaveSamePrefix(contactNumberPrefix,contactNumber))
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+	}
+};
---
+++ contactManager.cpp
@@ -0,0 +1,115 @@
+#include<bits/stdc++.h>
+#include "Services.cpp"
+using namespace std;
+
+struct Trie
+{
+	vector<Contact> containsWord;
+	Trie* children[26];
+	
+	Trie()
+	{
+		for(int i=0;i<26;i++)
+		{
+			children[i]=NULL;
+		}
+		containsWord.clear();
+	}
+};
+
+// Inhertince
+
+class contactManager:public Services
+{
+	Trie* root;
+	vector<Contact> result;
+	
+	public:
+	
+	contactManager()
+	{
+		root=new Trie();
+	}
+	
+	// this function is used to add a new contact in contact list
+	void addContact(string firstName,string lastName,string contactNumber)
+	{
+		if(!checkValidContactNumber(contactNumber) or !checkValidName(firstName) or !checkValidName(lastName))
+		{
+			return;
+		}
+		if(firstName.length()==0)
+		{
+			return;
+		}
+		
+		Contact person=Contact(firstName,lastName,contactNumber);
+		
+		int firstNameLength=firstName.length();
+		Trie* node=root;
+		
+		
+		for(int i=0;i<firstNameLength;i++)
+		{
+			char currentCharacter=firstName[i];
+			currentCharacter=convertToLowerCase(currentCharacter);
+			int indexOfCurrentCharacter=currentCharacter-'a';
+			
+			if(node->children[indexOfCurrentCharacter]==NULL)
+			{
+				node->children[indexOfCurrentCharacter]=new Trie();
+			}
+			node=node->children[indexOfCurrentCharacter];
+		}
+		node->containsWord.push_back(person);
+	}
+	
+	
+	void searchInSubtree(Trie* node,Contact prefixContact)
+	{
+		for(auto contactDetails:node->containsWord)
+		{
+			if(isTheContactMatching(contactDetails,prefixContact))
+			{
+				result.push_back(contactDetails);
+			}
+		}
+		for(int i=0;i<26;i++)
+		{
+			if(node->children[i]!=NULL)
+			{
+				searchInSubtree(node->children[i],prefixContact);
+			}
+		}
+	}
+	
+	
+	// first we will locate the first name up to prefix 
+	// and then we will find all the name under that subtree
+	vector<Contact> searchContact(string firstNamePrefix,string lastNamePrefix,string contactNumberPrefix)
+	{
+		result.clear();
+		Trie* node=root;
+		int firstNamePrefixLength=firstNamePrefix.length();
+		for(int i=0;i<firstNamePrefixLength;i++)
+		{
+			char currentCharacter=firstNamePrefix[i];
+			currentCharacter=convertToLowerCase(currentCharacter);
+			int indexOfCurrentCharacter=currentCharacter-'a';
+			
+			
+			if(node->children[indexOfCurrentCharacter]==NULL)
+			{
+				return result;
+			}
+			node=node->children[indexOfCurrentCharacter];
+		}
+		
+		// now we have got the node from which we can apply dfs to find
+		// all the person satisifying the condition
+		
+		Contact prefixContact=Contact(firstNamePrefix,lastNamePrefix,contactNumberPrefix);
+		searchInSubtree(node,prefixContact);
+		return result;
+	}
+};
