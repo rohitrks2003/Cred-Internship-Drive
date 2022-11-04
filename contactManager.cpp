d2a40d7805137f5b3c6d0aba5d2b114cf65883db
rohitrks2003 <75920133+rohitrks2003@users.noreply.github.com>
2022-11-04 06:28:34 +0530

    Update contactManager.cpp

--- contactManager.cpp
+++ contactManager.cpp
@@ -1,66 +1,38 @@
 #include<bits/stdc++.h>
-#include "Services.cpp"
+#include "Trie.cpp"
 using namespace std;
 
 
-// Struct to support Trie datatype
-struct Trie
+class contactManager:public Trie
 {
-	vector<Contact> containsWord;
-	Trie* children[26];
-	
-	Trie()
-	{
-		for(int i=0;i<26;i++)
-		{
-			children[i]=NULL;
-		}
-		containsWord.clear();
-	}
-};
-
-// Inheritence
-
-class contactManager:public Services
-{
-	Trie* root;
+	Trie* rootOfTrieOnFirstName;
+	Trie* rootOfTrieOnLastName;
+	Trie* rootOfTrieOnContactNumber;
+	set<vector<string>> checkDistinct;
 	vector<Contact> result;
 	
 	public:
 	
 	contactManager()
 	{
-		root=new Trie();
+		rootOfTrieOnFirstName=new Trie();
+		rootOfTrieOnLastName=new Trie();
+		rootOfTrieOnContactNumber=new Trie();
 	}
 	
 	// this function is used to add a new contact in contact list
 	void addContact(string firstName,string lastName,string contactNumber)
 	{
-		if(!checkValidContactNumber(contactNumber) or !checkValidName(firstName) or !checkValidName(lastName) or firstName.length()==0)
+		vector<string> personDetails={firstName,lastName,contactNumber};
+		if(!checkValidContactNumber(contactNumber) or !checkValidName(firstName) or !checkValidName(lastName) or firstName.length()==0 or checkDistinct.find(personDetails)!=checkDistinct.end())
 		{
 			return;
 		}
-		
 		Contact person=Contact(firstName,lastName,contactNumber);
-		
-		int firstNameLength=firstName.length();
-		Trie* node=root;
-		
-		
-		for(int i=0;i<firstNameLength;i++)
-		{
-			char currentCharacter=firstName[i];
-			currentCharacter=convertToLowerCase(currentCharacter);
-			int indexOfCurrentCharacter=currentCharacter-'a';
-			
-			if(node->children[indexOfCurrentCharacter]==NULL)
-			{
-				node->children[indexOfCurrentCharacter]=new Trie();
-			}
-			node=node->children[indexOfCurrentCharacter];
-		}
-		node->containsWord.push_back(person);
-		//cout<<"INFO: Contact Successfully Added\n";
+		checkDistinct.insert(personDetails);
+		rootOfTrieOnFirstName->insert(firstName,person);
+		rootOfTrieOnLastName->insert(lastName,person);
+		rootOfTrieOnContactNumber->insert(contactNumber,person);
 	}
 	
 	// This is a helper function that will help in the searching process
@@ -82,28 +54,15 @@ class contactManager:public Services
 		}
 	}
 	
-	
-	// first we will locate the first name up to prefix 
-	// and then we will find all the name under that subtree
+	// first we will locate the first name up to prefix and then we will find all the name under that subtree
 	vector<Contact> searchContactByPrefix(string firstNamePrefix,string lastNamePrefix,string contactNumberPrefix)
 	{
 		result.clear();
-		Trie* node=root;
-		int firstNamePrefixLength=firstNamePrefix.length();
-		for(int i=0;i<firstNamePrefixLength;i++)
+		Trie* node=rootOfTrieOnFirstName->pointerAtEndOfString(firstNamePrefix);
+		if(node==NULL)
 		{
-			char currentCharacter=firstNamePrefix[i];
-			currentCharacter=convertToLowerCase(currentCharacter);
-			int indexOfCurrentCharacter=currentCharacter-'a';
-			
-			
-			if(node->children[indexOfCurrentCharacter]==NULL)
-			{
-				return result;
-			}
-			node=node->children[indexOfCurrentCharacter];
+			return result;
 		}
-		
 		// now we have got the node from which we can apply dfs to find
 		// all the person satisifying the condition
 		
@@ -112,29 +71,31 @@ class contactManager:public Services
 		return result;
 	}
 	
-	vector<Contact> searchContactByExactName(string firstNamePrefix,string lastNamePrefix,string contactNumberPrefix)
+	vector<Contact> searchContactByExactName(string firstName,string lastName,string contactNumber)
 	{
-		result.clear();
-		Trie* node=root;
-		int firstNamePrefixLength=firstNamePrefix.length();
-		for(int i=0;i<firstNamePrefixLength;i++)
+		if(firstName=="" and lastName=="" and contactNumber=="")
 		{
-			char currentCharacter=firstNamePrefix[i];
-			currentCharacter=convertToLowerCase(currentCharacter);
-			int indexOfCurrentCharacter=currentCharacter-'a';
-			
-			
-			if(node->children[indexOfCurrentCharacter]==NULL)
-			{
-				return result;
-			}
-			node=node->children[indexOfCurrentCharacter];
+			return searchContactByPrefix(firstName,lastName,contactNumber);
+		}
+		result.clear();
+		vector<Contact> contactsToCheck;
+		if(firstName!="")
+		{
+			contactsToCheck=rootOfTrieOnFirstName->getTheContactsAtTheEndOfTheString(firstName);
+		}
+		else if(lastName!="")
+		{
+			contactsToCheck=rootOfTrieOnLastName->getTheContactsAtTheEndOfTheString(lastName);
+		}
+		else 
+		{
+			contactsToCheck=rootOfTrieOnContactNumber->getTheContactsAtTheEndOfTheString(contactNumber);
 		}
 		
-		Contact prefixContact=Contact(firstNamePrefix,lastNamePrefix,contactNumberPrefix);
-		for(auto contactDetails:node->containsWord)
+		Contact desiredContact=Contact(firstName,lastName,contactNumber);
+		for(auto contactDetails:contactsToCheck)
 		{
-			if(isTheContactMatching(contactDetails,prefixContact))
+			if(isTheContactMatching(contactDetails,desiredContact))
 			{
 				result.push_back(contactDetails);
 			}
